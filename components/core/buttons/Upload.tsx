7e7ba469a094ed3dcd44eebce3042b6ec5695c0d
Lu <luca@lus-macbook-pro.lan>
2024-03-14 21:35:16 -0300

    3 commit

--- components/core/buttons/Upload.tsx
+++ components/core/buttons/Upload.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { config } from '@/extra/config'
-import { DolarType } from '@/types/includes'
 import { useRouter } from 'next/navigation'
 import { useEffect, useRef, useState } from 'react'
 import { BsPlus } from 'react-icons/bs'
